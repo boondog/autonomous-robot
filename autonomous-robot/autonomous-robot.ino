6c79908a9d59f830e7bf759babf122eb01bdb684
Brian Bailey <bbailey@icaunus.com>
2015-12-02 19:22:28 -0500

    Working 3 sensor autonomy with beeping

--- autonomous-robot/autonomous-robot.ino
+++ autonomous-robot/autonomous-robot.ino
@@ -10,9 +10,11 @@
 * Include statements - include extra code we need
 *******************************************************/
 // This line includes the code we need to control servos
-#include <Servo.h>
+//#include <Servo.h>
 // This line includes the code we need to use the ultrasonic range finder
 #include <NewPing.h>
+
+#include <toneAC.h>
 
 #include <SoftwareSerial.h>
 #include <SabertoothSimplified.h>
@@ -21,8 +23,13 @@
 /*******************************************************
 * Define statements - Define constants we will use later
 *******************************************************/
-#define TRIGGER_PIN       A0    // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN          A1    // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define CENTER_TRIGGER_PIN        A0    // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define CENTER_ECHO_PIN           A1    // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define RIGHT_TRIGGER_PIN         6     // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define RIGHT_ECHO_PIN            7     // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define LEFT_TRIGGER_PIN          5     // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define LEFT_ECHO_PIN             4     // Arduino pin tied to echo pin on the ultrasonic sensor.
+
 #define LEFT_WHEEL_PIN    5     // Arduino pin tied to the left servo wheel motor
 #define RIGHT_WHEEL_PIN   6     // Arduino pin tied to the right servo wheel motor
 #define MAX_DISTANCE      400   // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
@@ -35,18 +42,22 @@
 #define MAX_POSITIVE_SPEED   96
 #define MAX_NEGATIVE_SPEED   -96
 
-
 /********************************************************
 * Class object instances - These will help us control the
 * sensors and motors.
 ********************************************************/
 // NewPing setup of pins and maximum distance.
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+NewPing sonar(CENTER_TRIGGER_PIN, CENTER_ECHO_PIN, MAX_DISTANCE);
+
+NewPing left_sonar(LEFT_TRIGGER_PIN, LEFT_ECHO_PIN, MAX_DISTANCE);
+
+NewPing right_sonar(RIGHT_TRIGGER_PIN, RIGHT_ECHO_PIN, MAX_DISTANCE);
+
 
 // Servo motor control objects
 // Up to twelve servo objects can be created and controlled
-Servo rightWheel;
-Servo leftWheel;
+//Servo rightWheel;
+//Servo leftWheel;
 
 // Setup the serial communication
 SoftwareSerial SWSerial(NOT_A_PIN, SABER_SERIAL_PIN); // RX on no pin (unused), TX on pin 11 (to S1).
@@ -66,14 +77,8 @@ void setup()
   SWSerial.begin(9600);
 
   // Tell the servo objects which pins the servos are connected to
-  leftWheel.attach(LEFT_WHEEL_PIN, 1000, 2000);
-  rightWheel.attach(RIGHT_WHEEL_PIN, 1000, 2000);
-
-  // Calibration
-  //leftWheel.write(90);
-  //rightWheel.write(90);
-  // Wait for motor controller to calibrate
-  // delay(3000);
+//  leftWheel.attach(LEFT_WHEEL_PIN, 1000, 2000);
+//  rightWheel.attach(RIGHT_WHEEL_PIN, 1000, 2000);
 
 }
 
@@ -82,21 +87,55 @@ void setup()
 ********************************************************/
 void loop()
 {
+
+//  talk();
  
   // Variables we will need in the loop code
   int distInCentimeters;  // We'll store the ultrasonic range distance here
+  int leftDistInCent;     // We'll store the ultrasonic range distance here
+  int rightDistInCent;    // We'll store the ultrasonic range distance here
 
   /*******************************
   * Ultrasonic Range Finder Code
   ********************************/
   // This code takes a distance reading from the range finder
-  distInCentimeters = ping_cm_BugFix();
+  distInCentimeters = ping_cm_BugFix(sonar, CENTER_ECHO_PIN);
+  leftDistInCent    = ping_cm_BugFix(left_sonar, LEFT_ECHO_PIN);
+  rightDistInCent   = ping_cm_BugFix(right_sonar, RIGHT_ECHO_PIN);
 
   // Print distance for debug (0 = outside set distance range)
-  Serial.print("Ping: ");
+  Serial.print("Center: ");
   Serial.print(distInCentimeters);
   Serial.println("cm");
 
+  Serial.print("Left: ");
+  Serial.print(leftDistInCent);
+  Serial.println("cm");
+
+  Serial.print("Right: ");
+  Serial.print(rightDistInCent);
+  Serial.println("cm");
+
+  // If there's something left nudge right
+  if(leftDistInCent < 10)
+  {
+    Serial.println("Right nudge");
+    ST.motor(LEFT_WHEEL, MAX_POSITIVE_SPEED);
+    ST.motor(RIGHT_WHEEL, MAX_POSITIVE_SPEED);
+    delay(300);
+    ST.motor(LEFT_WHEEL,  MAX_NEGATIVE_SPEED);
+    ST.motor(RIGHT_WHEEL, MAX_POSITIVE_SPEED);
+  }
+  // If there's somethign right nudge left
+  if(rightDistInCent < 10)
+  {
+    Serial.println("Left nudge");
+    ST.motor(LEFT_WHEEL, MAX_NEGATIVE_SPEED);
+    ST.motor(RIGHT_WHEEL, MAX_NEGATIVE_SPEED);
+    delay(300);
+    ST.motor(LEFT_WHEEL,  MAX_NEGATIVE_SPEED);
+    ST.motor(RIGHT_WHEEL, MAX_POSITIVE_SPEED);    
+  }
   // If don't see an obsticle keep driving
   if(distInCentimeters > 30 || distInCentimeters == 0){
     Serial.println("Driving");
@@ -113,28 +152,63 @@ void loop()
       ST.motor(RIGHT_WHEEL, MAX_POSITIVE_SPEED);
       //leftWheel.write(180);
       //rightWheel.write(180);
-                  distInCentimeters = ping_cm_BugFix();
+                  distInCentimeters = ping_cm_BugFix(sonar, CENTER_ECHO_PIN);
                   Serial.print("Seeing things ");
                   Serial.print(distInCentimeters);
                   Serial.println(" cm away");
                 }
                 Serial.println("Looks clear!");
   }
+
+  execute_random();
+
 }
 
+void execute_random()
+{
+  if(random(0,100) == 1)
+  {
+      Serial.println("Disco Time!!");
+      ST.motor(RIGHT_WHEEL, MAX_POSITIVE_SPEED);
+      ST.motor(LEFT_WHEEL, MAX_POSITIVE_SPEED);    
+      delay(random(500,1500));
+  }
+
+  if(random(0,200) == 2){
+    ST.motor(RIGHT_WHEEL, -25);
+    ST.motor(LEFT_WHEEL, -25);
+    talk();
+    ST.motor(RIGHT_WHEEL, 25);
+    ST.motor(LEFT_WHEEL, 25);
+    talk();
+  }
+}
 // This is a wrapper function that tries to avoid a bug with the
 // HC-SR04 modules where they can get stuck return zero forever
-int ping_cm_BugFix(){
+int ping_cm_BugFix(NewPing sensor, int echo_pin){
   
-        int distCm = sonar.ping_cm();
+        int distCm = sensor.ping_cm();
         
         if(distCm == 0){
           delay(100);
-          pinMode(ECHO_PIN, OUTPUT);
-          digitalWrite(ECHO_PIN, LOW);
+          pinMode(echo_pin, OUTPUT);
+          digitalWrite(echo_pin, LOW);
           delay(100);
-          pinMode(ECHO_PIN, INPUT);
+          pinMode(echo_pin, INPUT);
         }
 
         return distCm;
 }
+
+void talk(){
+  int t,i;
+  t = random(65, 250);
+  for(i=0;i<random(3,12);i++){
+     toneAC(t+random(0,5)*t);
+     delay(random(100,200));
+     delay(random(100,200)*1.3);
+     toneAC(0);
+  }
+}
+
+
